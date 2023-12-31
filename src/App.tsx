d623b3bd84b9a93561276b91e08d013e8e42ed0f
“evan-weissrussland” <evanwodichka@gmail.com>
2023-12-31 19:00:30 +0300

    new

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,14 @@
+import { Button } from '@/components/ui/button'
+
 export function App() {
-  return <div>Hello</div>
+  return (
+    <div>
+      <Button as={'a'} href={'/link'} variant={'primary'}>
+        Hello
+      </Button>
+      <Button as={'button'} variant={'primary'}>
+        Hello
+      </Button>
+    </div>
+  )
 }
