1920ccfcb6393b6c00bcf4f76a16ef2cc92e0849
“evan-weissrussland” <evanwodichka@gmail.com>
2024-01-10 23:45:22 +0300

    селект не получается сделать

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,10 @@ import { Button } from '@/components/ui/button'
 import { CheckboxComponent } from '@/components/ui/checkbox'
 import { Input } from '@/components/ui/input'
 import { SelectComponent } from '@/components/ui/select'
+import { Typography } from '@/components/ui/typography'
+import * as Select from '@radix-ui/react-select'
+
+import s from '@/components/ui/select/select.module.scss'
 
 export function App() {
   return (
--- src/components/ui/select/select.tsx
+++ src/components/ui/select/select.tsx
@@ -19,40 +19,63 @@ export type SelectProps<T extends ElementType = 'button'> = {
   as?: T
   children?: ReactNode
   className?: string
+  disabled?: boolean
   variant?: VariantType
 } & ComponentPropsWithoutRef<T>
 
 export const SelectComponent = <T extends ElementType = 'button'>(
   props: SelectProps<T> & Omit<ComponentPropsWithoutRef<T>, keyof SelectProps<T>>
 ) => {
-  const { as: Component = 'button', children, className = '', variant = 'Body 1', ...rest } = props
-  const placeholderValue = <Typography variant={variant}>Select-box</Typography>
+  const {
+    as: Component = 'button',
+    children,
+    className = '',
+    disabled = false,
+    variant = 'Body 1',
+    ...rest
+  } = props
+  const placeholderValue = (
+    <Typography variant={variant}>
+      <span className={disabled ? s.disabled : ''}>Select-box</span>
+    </Typography>
+  )
 
   return (
-    <Select.Root>
-      <Select.Trigger className={s.selectTrigger}>
-        <Select.Value placeholder={placeholderValue} />
-      </Select.Trigger>
+    <div style={{ display: 'inline-block' }}>
+      <Typography variant={'Body 2'}>
+        <span className={`${disabled && s.disabled} ${s.spanLabel}`}>Select-box</span>
+      </Typography>
+      <Select.Root disabled={disabled}>
+        <Select.Trigger
+          className={`${disabled && s.disabled} ${s.selectTrigger}`}
+          disabled={disabled}
+        >
+          <Select.Value placeholder={placeholderValue} />
+        </Select.Trigger>
 
-      <Select.Portal>
-        <Select.Content className={s.selectContent}>
-          <Select.ScrollUpButton />
-          <Select.Viewport className={s.selectViewport}>
-            <Select.Item className={s.selectItem} value={'dsv1'}>
-              <Typography variant={variant}>ssgsdg</Typography>
-            </Select.Item>
-            <Select.Item className={s.selectItem} value={'dsv2'}>
-              <Typography variant={variant}>dfssgbadb</Typography>
-            </Select.Item>
-            <Select.Item className={s.selectItem} value={'dsv3'}>
-              <Typography variant={variant}>djrtjadb</Typography>
-            </Select.Item>
-          </Select.Viewport>
-          <Select.ScrollDownButton />
-          <Select.Arrow />
-        </Select.Content>
-      </Select.Portal>
-    </Select.Root>
+        <Select.Portal>
+          <Select.Content className={s.selectContent}>
+            <Select.ScrollUpButton />
+            <Select.Viewport className={s.selectViewport}>
+              <Select.Item className={s.selectItem} value={'dsv1'}>
+                <Typography variant={variant}>ssgsdg</Typography>
+                <Select.ItemText>
+                  <Typography variant={variant}>ssgsdg</Typography>
+                </Select.ItemText>
+              </Select.Item>
+              <Select.Item className={s.selectItem} value={'dfssgbadb'}>
+                <Typography variant={variant}>dfssgbadb</Typography>
+              </Select.Item>
+              <Select.Item className={s.selectItem} value={'dsv3'}>
+                <Typography variant={variant}>djrtjadb</Typography>
+              </Select.Item>
+            </Select.Viewport>
+            <Select.ScrollDownButton />
+            <Select.Arrow />
+          </Select.Content>
+        </Select.Portal>
+      </Select.Root>
+    </div>
   )
 }
 
@@ -70,3 +93,5 @@ const SelectItem: FC<SelectItemType> = forwardRef(({ children, ...props }, forwa
     </Select.Item>
   )
 })*/
+
+//TODO не знаю как сделать селект. По стилизации вроде всё совпадает, то при выборе из меню селекта, селект схлопывается.
