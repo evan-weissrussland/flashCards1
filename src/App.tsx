b24bd39c7eab20454303bd954e291123db69b3cd
“evan-weissrussland” <evanwodichka@gmail.com>
2024-01-10 22:20:51 +0300

    рефакторинг

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
 import { Button } from '@/components/ui/button'
 import { CheckboxComponent } from '@/components/ui/checkbox'
 import { Input } from '@/components/ui/input'
+import { SelectComponent } from '@/components/ui/select'
 
 export function App() {
   return (
-    <div>
-      <Button as={'a'} href={'https://google.com'}>
-        Hello
-      </Button>
-      <Button>Button primary</Button>
-      <Input type={'search'} />
-      <CheckboxComponent variant={'Body 2'}>asdvdhhsd</CheckboxComponent>
-    </div>
+    <>
+      <div>
+        <Button as={'a'} href={'https://google.com'}>
+          Hello
+        </Button>
+        <Button>Button primary</Button>
+        <Input type={'search'} />
+        <CheckboxComponent variant={'Body 2'}>asdvdhhsd</CheckboxComponent>
+      </div>
+      <div>
+        <SelectComponent />
+      </div>
+    </>
   )
 }
