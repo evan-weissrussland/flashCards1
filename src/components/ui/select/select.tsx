b2b4f05ae1ab7a9de158423be839139ee0c90759
“evan-weissrussland” <evanwodichka@gmail.com>
2024-01-11 22:03:03 +0300

    селект почти сделан. Не получилось только изменить цвет при выделении item'а

--- src/components/ui/select/select.tsx
+++ src/components/ui/select/select.tsx
@@ -1,19 +1,9 @@
-import {
-  ComponentPropsWithoutRef,
-  ElementType,
-  FC,
-  ReactNode,
-  RefAttributes,
-  forwardRef,
-} from 'react'
+import { ComponentPropsWithoutRef, ElementType, ReactNode } from 'react'
 
 import { Typography, VariantType } from '@/components/ui/typography'
 import * as Select from '@radix-ui/react-select'
-import { SelectItemProps } from '@radix-ui/react-select'
 
 import s from './select.module.scss'
-
-import IntrinsicAttributes = React.JSX.IntrinsicAttributes
 
 export type SelectProps<T extends ElementType = 'button'> = {
   as?: T
@@ -31,7 +21,7 @@ export const SelectComponent = <T extends ElementType = 'button'>(
     children,
     className = '',
     disabled = false,
-    variant = 'Body 1',
+    variant = 'Body 2',
     ...rest
   } = props
   const placeholderValue = (
@@ -55,23 +45,20 @@ export const SelectComponent = <T extends ElementType = 'button'>(
 
         <Select.Portal>
           <Select.Content className={s.selectContent}>
-            <Select.ScrollUpButton />
-            <Select.Viewport className={s.selectViewport}>
-              <Select.Item className={s.selectItem} value={'dsv1'}>
+            <Select.Item
+              className={`${s.selectItem} ${disabled && s.disabled}`}
+              disabled
+              value={'dsv1'}
+            >
+              <Select.ItemText>
                 <Typography variant={variant}>ssgsdg</Typography>
-                <Select.ItemText>
-                  <Typography variant={variant}>ssgsdg</Typography>
-                </Select.ItemText>
-              </Select.Item>
-              <Select.Item className={s.selectItem} value={'dfssgbadb'}>
-                <Typography variant={variant}>dfssgbadb</Typography>
-              </Select.Item>
-              <Select.Item className={s.selectItem} value={'dsv3'}>
-                <Typography variant={variant}>djrtjadb</Typography>
-              </Select.Item>
-            </Select.Viewport>
-            <Select.ScrollDownButton />
-            <Select.Arrow />
+              </Select.ItemText>
+            </Select.Item>
+            <Select.Item className={s.selectItem} value={'dfssgbadb'}>
+              <Select.ItemText>
+                <Typography variant={variant}>wef</Typography>
+              </Select.ItemText>
+            </Select.Item>
           </Select.Content>
         </Select.Portal>
       </Select.Root>
@@ -79,19 +66,4 @@ export const SelectComponent = <T extends ElementType = 'button'>(
   )
 }
 
-/*type SelectItemType = {
-  children?: ReactNode
-  value: string
-} & IntrinsicAttributes &
-  SelectItemProps &
-  RefAttributes<HTMLDivElement>
-
-const SelectItem: FC<SelectItemType> = forwardRef(({ children, ...props }, forwardedRef) => {
-  return (
-    <Select.Item className={s.selectItem} {...props} ref={forwardedRef}>
-      <Select.ItemText>{children}</Select.ItemText>
-    </Select.Item>
-  )
-})*/
-
-//TODO не знаю как сделать селект. По стилизации вроде всё совпадает, то при выборе из меню селекта, селект схлопывается.
+//TODO 1. Нужно использовать Select.ItemText; 2. Чтобы при раскрытии item'ов выбранный item не перекрывал trigger, нужно удалить Select.Viewport
