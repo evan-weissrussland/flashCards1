4d1e4b7ce96552fe790debe94b95edca7a0afcaf
“evan-weissrussland” <evanwodichka@gmail.com>
2024-02-01 22:28:14 +0300

    refactor

--- src/app/ui/App.tsx
+++ src/app/ui/App.tsx
@@ -3,7 +3,6 @@ import { Outlet } from 'react-router-dom'
 
 import { PrivateRouter } from '@/app/model/router'
 import { Header } from '@/app/ui/Header/ui'
-import { Modalka } from '@/common/components/modal'
 import { useAuthMeQuery } from '@/features/Auth/api/authMe-api'
 
 export const Context = createContext('')
