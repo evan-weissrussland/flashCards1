3f48a8a0bb05aff734e596eaf3449739b5601179
“evan-weissrussland” <evanwodichka@gmail.com>
2024-02-16 23:11:59 +0300

    add custom hook router context and spinner

--- src/app/ui/App.tsx
+++ src/app/ui/App.tsx
@@ -2,36 +2,21 @@ import { Outlet } from 'react-router-dom'
 
 import { PrivateRouter } from '@/app/model/router'
 import { Header } from '@/app/ui/Header/ui'
-import { Responce } from '@/features/Auth/api/authMe-api'
-import { useAuthMeQuery } from '@/features/Auth/api/authMe-api'
+import { Spinner } from '@/app/ui/Spinner/Spinner'
+import { Responce, useAuthMeQuery } from '@/features/Auth/api/authMe-api'
+export type MyIdType = {
+  myId: string
+}
 
 export function App() {
   const { data, isLoading } = useAuthMeQuery()
-  const loadingHTML = isLoading ? (
-    <div
-      style={{
-        alignItems: 'center',
-        background: 'rgba(0, 0, 0, 0.75)',
-        display: 'flex',
-        height: '100vh',
-        justifyContent: 'center',
-        position: 'absolute',
-        width: '100%',
-        zIndex: '100',
-      }}
-    >
-      <span style={{ color: 'green', fontSize: '100px', fontWeight: 'bold' }}> ...Loading </span>
-    </div>
-  ) : (
-    <></>
-  )
 
   return (
-    <div style={{ position: 'relative' }}>
-      {loadingHTML}
+    <div>
+      {isLoading && <Spinner />}
       <Header data={data as Responce} />
-      <Outlet />
-      <PrivateRouter data={data as Responce} />
+      <Outlet context={{ myId: data?.id as string } satisfies MyIdType} />
+      <PrivateRouter data={data as Responce} isLoading={isLoading} />
     </div>
   )
 }
