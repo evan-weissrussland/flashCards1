3a156c851be1a2ff1df71488404b37bb84b4af1e
“evan-weissrussland” <evanwodichka@gmail.com>
2024-02-22 20:01:56 +0300

    вынес таблицу из Decks в отдельный компонент

---
+++ src/common/components/tableDecks/Table.tsx
@@ -0,0 +1,157 @@
+import { FC, useCallback, useMemo } from 'react'
+import { useNavigate } from 'react-router-dom'
+
+import { Button } from '@/common/components/button'
+import { SortAscIcon, SortDescIcon } from '@/common/icons/SortAsc'
+import { Deck } from '@/features/Decks/api/getDecks'
+import { ModalDeleteDeck } from '@/features/Decks/ui/ModalDeleteDeck'
+import { ModalEditDeck } from '@/features/Decks/ui/ModalEditDeck'
+import { useAuthContext } from '@/hooks/hooks'
+
+type Props = {
+  directionSort: 'asc' | 'desc' | null
+  items: Deck[] | undefined
+  setDirectionSort: (
+    v: ('asc' | 'desc' | null) | ((dir: 'asc' | 'desc' | null) => 'asc' | 'desc' | null)
+  ) => void
+  setSortBy: (v: 'author.name' | 'cardsCount' | 'created' | 'name' | 'updated' | null) => void
+  sortBy: 'author.name' | 'cardsCount' | 'created' | 'name' | 'updated' | null
+}
+export const TableDeck: FC<Props> = props => {
+  const { directionSort, items, setDirectionSort, setSortBy, sortBy } = props
+
+  //получаем мой ID юзера из контекста (Арр)
+  const { myId } = useAuthContext()
+
+  //функция для изменения URL
+  const navigate = useNavigate()
+
+  //переход на страницу выбранной колоды
+  const navigateToDeckHandler = useCallback((id: string) => {
+    navigate(`/decks/${id}`)
+  }, [])
+
+  //обработчик изменения сортировки. Если по полю ранее кликали, то меняется направление сортировки, если по полю клюкнули впервые, то задаётся напрвление сортировки
+  const sortByHandler = useCallback(
+    (v: 'author.name' | 'cardsCount' | 'created' | 'name' | 'updated') => {
+      if (v === sortBy) {
+        setDirectionSort((dir: 'asc' | 'desc' | null) => {
+          if (dir === 'asc') {
+            return 'desc'
+          }
+          if (dir === null) {
+            return 'asc'
+          }
+
+          return null
+        })
+      }
+
+      if (v !== sortBy) {
+        setSortBy(v)
+        setDirectionSort('asc')
+      }
+    },
+    [setDirectionSort, setSortBy, sortBy]
+  )
+
+  //отрисовываем таблицу из карт с сервера
+  const table = useMemo(
+    () =>
+      items?.map(it => {
+        return (
+          <tr key={it.id} style={{ padding: '6px 24px' }}>
+            <td style={{ alignItems: 'center', display: 'flex', gap: '10px' }}>
+              <img
+                alt={' '}
+                onClick={() => {
+                  navigateToDeckHandler(it.id)
+                }}
+                src={it.cover}
+                style={{ borderRadius: '2px', cursor: 'pointer', height: '48px', width: '118px' }}
+              />
+              <div
+                onClick={() => {
+                  navigateToDeckHandler(it.id)
+                }}
+                style={{ cursor: 'pointer' }}
+              >
+                {it.name}
+              </div>
+            </td>
+            <td>{it.cardsCount}</td>
+            <td>{new Date(it.updated).toLocaleString('ru-RU')}</td>
+            <td>{it.author.name}</td>
+            {it.userId === myId && (
+              <td>
+                <ModalEditDeck
+                  deckCover={it.cover}
+                  deckId={it.id}
+                  deckIsPrivate={it.isPrivate}
+                  deckName={it.name}
+                >
+                  <Button className={'padding4px'} icon={'edit'} variant={'secondary'} />
+                </ModalEditDeck>
+                <ModalDeleteDeck deckName={it.name} idDeck={it.id}>
+                  <Button className={'padding4px'} icon={'delete'} variant={'secondary'} />
+                </ModalDeleteDeck>
+              </td>
+            )}
+          </tr>
+        )
+      }),
+    [items, navigateToDeckHandler, myId]
+  )
+
+  return (
+    <div>
+      <table style={{ width: '100%' }}>
+        <thead>
+          <tr>
+            <th
+              onClick={() => {
+                sortByHandler('name')
+              }}
+              style={{ color: 'red', textAlign: 'start' }}
+            >
+              Name {'name' === sortBy && directionSort === 'asc' && <SortAscIcon />}
+              {'name' === sortBy && directionSort === 'desc' && <SortDescIcon />}
+            </th>
+            <th
+              onClick={() => {
+                sortByHandler('cardsCount')
+              }}
+              style={{
+                color: 'red',
+                textAlign: 'start',
+              }}
+            >
+              Cards {'cardsCount' === sortBy && directionSort === 'asc' && <SortAscIcon />}
+              {'cardsCount' === sortBy && directionSort === 'desc' && <SortDescIcon />}
+            </th>
+            <th
+              onClick={() => {
+                sortByHandler('updated')
+              }}
+              style={{ color: 'red', textAlign: 'start' }}
+            >
+              Last Updated {'updated' === sortBy && directionSort === 'asc' && <SortAscIcon />}
+              {'updated' === sortBy && directionSort === 'desc' && <SortDescIcon />}
+            </th>
+            <th
+              onClick={() => {
+                sortByHandler('created')
+              }}
+              style={{ color: 'red', textAlign: 'start' }}
+            >
+              Created by {'created' === sortBy && directionSort === 'asc' && <SortAscIcon />}
+              {'created' === sortBy && directionSort === 'desc' && <SortDescIcon />}
+            </th>
+            <th style={{ color: 'red', textAlign: 'start' }}></th>
+          </tr>
+        </thead>
+        <tbody>{table}</tbody>
+      </table>
+    </div>
+  )
+}
--- src/features/Decks/ui/Decks.tsx
+++ src/features/Decks/ui/Decks.tsx
@@ -1,5 +1,4 @@
-import { useCallback, useMemo, useState } from 'react'
-import { useNavigate } from 'react-router-dom'
+import { useCallback, useState } from 'react'
 
 import { Spinner } from '@/app/ui/Spinner/Spinner'
 import { Button } from '@/common/components/button'
@@ -7,20 +6,16 @@ import { Input } from '@/common/components/input'
 import { PageSizeType, Paginator } from '@/common/components/paginator/paginator'
 import { RangeSlider } from '@/common/components/slider'
 import { Tabs, TabsList, TabsTrigger } from '@/common/components/tabSwitcher'
+import { TableDeck } from '@/common/components/tableDecks/Table'
 import { Typography } from '@/common/components/typography'
-import { SortAscIcon, SortDescIcon } from '@/common/icons/SortAsc'
 import { useGetDecksQuery, useGetMinMaxAmoundCardsQuery } from '@/features/Decks/api/getDecks'
 import { ModalAddNewDeck } from '@/features/Decks/ui/ModalAddNewDeck'
-import { ModalDeleteDeck } from '@/features/Decks/ui/ModalDeleteDeck'
-import { ModalEditDeck } from '@/features/Decks/ui/ModalEditDeck'
 import { useAuthContext } from '@/hooks/hooks'
 
 export const Decks = () => {
   //получаем мой ID юзера из контекста (Арр)
   // const { data: meData } = useAuthMeQuery()
   const { myId: authMeId } = useAuthContext()
-  //функция для изменения URL
-  const navigate = useNavigate()
 
   //для изменения value инпута
   const [search, setSearch] = useState('')
@@ -76,11 +71,6 @@ export const Decks = () => {
   //хук RTK Query. Запрос на сервер за количеством min и max колод (Decks)
   const result = useGetMinMaxAmoundCardsQuery()
 
-  //переход на страницу выбранной колоды
-  const navigateToDeckHandler = useCallback((id: string) => {
-    navigate(`/decks/${id}`)
-  }, [])
-
   //зачистка фильтра
   const clearFilterHandler = useCallback(() => {
     setSearch('')
@@ -133,54 +123,6 @@ export const Decks = () => {
     [authMeId, setMyId, setAuthorCards]
   )
 
-  //отрисовываем таблицу из карт с сервера
-  const table = useMemo(
-    () =>
-      data?.items.map(it => {
-        return (
-          <tr key={it.id} style={{ padding: '6px 24px' }}>
-            <td style={{ alignItems: 'center', display: 'flex', gap: '10px' }}>
-              <img
-                alt={' '}
-                onClick={() => {
-                  navigateToDeckHandler(it.id)
-                }}
-                src={it.cover}
-                style={{ borderRadius: '2px', cursor: 'pointer', height: '48px', width: '118px' }}
-              />
-              <div
-                onClick={() => {
-                  navigateToDeckHandler(it.id)
-                }}
-                style={{ cursor: 'pointer' }}
-              >
-                {it.name}
-              </div>
-            </td>
-            <td>{it.cardsCount}</td>
-            <td>{new Date(it.updated).toLocaleString('ru-RU')}</td>
-            <td>{it.author.name}</td>
-            {it.userId === authMeId && (
-              <td>
-                <ModalEditDeck
-                  deckCover={it.cover}
-                  deckId={it.id}
-                  deckIsPrivate={it.isPrivate}
-                  deckName={it.name}
-                >
-                  <Button className={'padding4px'} icon={'edit'} variant={'secondary'} />
-                </ModalEditDeck>
-                <ModalDeleteDeck deckName={it.name} idDeck={it.id}>
-                  <Button className={'padding4px'} icon={'delete'} variant={'secondary'} />
-                </ModalDeleteDeck>
-              </td>
-            )}
-          </tr>
-        )
-      }),
-    [data?.items, navigateToDeckHandler, authMeId]
-  )
-
   //переменная, которой будет присвоена ошибка из хука RTKQ. Выводим её юзеру
   let narrowingError
 
@@ -198,30 +140,6 @@ export const Decks = () => {
       narrowingError = error.message
     }
   }
-
-  //обработчик изменения сортировки. Если по полю ранее кликали, то меняется направление сортировки, если по полю клюкнули впервые, то задаётся напрвление сортировки
-  const sortByHandler = useCallback(
-    (v: 'author.name' | 'cardsCount' | 'created' | 'name' | 'updated') => {
-      if (v === sortBy) {
-        setDirectionSort(dir => {
-          if (dir === 'asc') {
-            return 'desc'
-          }
-          if (dir === null) {
-            return 'asc'
-          }
-
-          return null
-        })
-      }
-
-      if (v !== sortBy) {
-        setSortBy(v)
-        setDirectionSort('asc')
-      }
-    },
-    [sortBy]
-  )
 
   return (
     <>
@@ -260,55 +178,13 @@ export const Decks = () => {
             Clear filter
           </Button>
         </div>
-        <div>
-          <table style={{ width: '100%' }}>
-            <thead>
-              <tr>
-                <th
-                  onClick={() => {
-                    sortByHandler('name')
-                  }}
-                  style={{ color: 'red', textAlign: 'start' }}
-                >
-                  Name {'name' === sortBy && directionSort === 'asc' && <SortAscIcon />}
-                  {'name' === sortBy && directionSort === 'desc' && <SortDescIcon />}
-                </th>
-                <th
-                  onClick={() => {
-                    sortByHandler('cardsCount')
-                  }}
-                  style={{
-                    color: 'red',
-                    textAlign: 'start',
-                  }}
-                >
-                  Cards {'cardsCount' === sortBy && directionSort === 'asc' && <SortAscIcon />}
-                  {'cardsCount' === sortBy && directionSort === 'desc' && <SortDescIcon />}
-                </th>
-                <th
-                  onClick={() => {
-                    sortByHandler('updated')
-                  }}
-                  style={{ color: 'red', textAlign: 'start' }}
-                >
-                  Last Updated {'updated' === sortBy && directionSort === 'asc' && <SortAscIcon />}
-                  {'updated' === sortBy && directionSort === 'desc' && <SortDescIcon />}
-                </th>
-                <th
-                  onClick={() => {
-                    sortByHandler('created')
-                  }}
-                  style={{ color: 'red', textAlign: 'start' }}
-                >
-                  Created by {'created' === sortBy && directionSort === 'asc' && <SortAscIcon />}
-                  {'created' === sortBy && directionSort === 'desc' && <SortDescIcon />}
-                </th>
-                <th style={{ color: 'red', textAlign: 'start' }}></th>
-              </tr>
-            </thead>
-            <tbody>{table}</tbody>
-          </table>
-        </div>
+        <TableDeck
+          directionSort={directionSort}
+          items={data?.items}
+          setDirectionSort={setDirectionSort}
+          setSortBy={setSortBy}
+          sortBy={sortBy}
+        />
         <div>
           <Paginator
             currentPage={data?.pagination.currentPage}
