434715e4b5b3f357588db7a126e8e426f9a3e0ee
“evan-weissrussland” <evanwodichka@gmail.com>
2024-02-24 10:39:51 +0300

    добавил в инпуте для пароля отображение пароля

--- src/common/components/input/input.tsx
+++ src/common/components/input/input.tsx
@@ -5,8 +5,10 @@ import {
   ReactNode,
   forwardRef,
   memo,
+  useState,
 } from 'react'
 
+import { IconsForInput } from '@/common/icons/IconsForInput'
 import clsx from 'clsx'
 
 import s from './input.module.scss'
@@ -43,6 +45,17 @@ export const Input: InputWithRef = memo(
     } = props
     const Component: ElementType = as || 'input'
 
+    const [showPass, setShowPass] = useState(false)
+
+    let newType = type
+
+    if (type === 'search') {
+      newType = 'text'
+    }
+    if (type === 'password' && showPass && !rest.disabled) {
+      newType = 'text'
+    }
+
     return (
       <div className={clsx(s.inputWrapper, s[className])} style={props.style}>
         <span className={`${s.label} ${rest.disabled && s.disabledLabel}`}>{label}</span>
@@ -53,9 +66,16 @@ export const Input: InputWithRef = memo(
             callback && callback(e.currentTarget.value)
           }}
           ref={ref}
-          type={type === 'search' ? 'text' : type}
+          type={newType}
           {...rest}
         />
+        {type === 'password' && (
+          <IconsForInput
+            disabled={rest.disabled}
+            onClick={() => setShowPass(!showPass)}
+            style={{ position: 'absolute', right: '14px', top: '34px' }}
+          />
+        )}
         <span className={s.errorText}>{error && `${error}!`}</span>
       </div>
     )
