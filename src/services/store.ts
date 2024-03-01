8bf7e2303f3a31d30bead699aed237687c89f20f
“evan-weissrussland” <evanwodichka@gmail.com>
2024-03-01 22:59:15 +0300

    добавил метод setupListeners(store.dispatch). как в видео от Валеры по RTKQ

--- src/services/store.ts
+++ src/services/store.ts
@@ -1,5 +1,6 @@
 import { baseApi } from '@/app/api/base-api'
 import { configureStore } from '@reduxjs/toolkit'
+import { setupListeners } from '@reduxjs/toolkit/query'
 
 export const store = configureStore({
   middleware: getDefaultMiddleware => getDefaultMiddleware().concat(baseApi.middleware),
@@ -7,6 +8,8 @@ export const store = configureStore({
     [baseApi.reducerPath]: baseApi.reducer,
   },
 })
+
+setupListeners(store.dispatch)
 
 export type AppDispatch = typeof store.dispatch
 export type RootState = ReturnType<typeof store.getState>
