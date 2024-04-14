d3cd82de75eb544c04563028508421710402785c
“evan-weissrussland” <evanwodichka@gmail.com>
2024-04-14 21:49:43 +0300

    добавил декоратор Provider store для истории ModalLearnToDeck

--- src/features/Decks/ui/ModalLearnToDeck/modal.stories.tsx
+++ src/features/Decks/ui/ModalLearnToDeck/modal.stories.tsx
@@ -1,10 +1,15 @@
 import type { Meta, StoryObj } from '@storybook/react'
 
+import { Provider } from 'react-redux'
+
+import { store } from '@/services/store'
+
 import { ModalLearnToDeck } from './ModalLearnToDeck'
 
 const meta = {
   argTypes: {},
   component: ModalLearnToDeck,
+  decorators: [story => <Provider store={store}>{story()}</Provider>],
   tags: ['autodocs'],
   title: 'Components/ModalLearnToDeck',
 } satisfies Meta<typeof ModalLearnToDeck>
